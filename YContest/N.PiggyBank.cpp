6e1f2c6cf22076383154dca4dbce3e4f4fa8ec71
alex-nb <285484@niuitmo.ru>
2021-06-24 04:14:27 +0300

    N solved

--- YContest/N.PiggyBank.cpp
+++ YContest/N.PiggyBank.cpp
@@ -12,3 +12,41 @@
  * пока не
  * движение наверх отдельный метод цикл вайл
  */
+
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+vector<int> pigs;
+vector<int> temp;
+int num = 0, ans = 0, n = 0, pig;
+
+void DFS(int v) {
+    if (pigs[v] != -1) {
+        if (pigs[v] == num)
+            ans++;
+        return;
+    }
+    pigs[v] = num;
+    DFS(temp[v]);
+}
+
+int main() {
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i)
+        pigs.push_back(-1);
+
+    for (int i = 0; i < n; i++) {
+        scanf(" %d", &pig);
+        temp.push_back(--pig);
+    }
+
+    for (int i = 0; i < n; i++) {
+        num = i;
+        DFS(i);
+    }
+    printf("%d", ans);
+    return 0;
+}
